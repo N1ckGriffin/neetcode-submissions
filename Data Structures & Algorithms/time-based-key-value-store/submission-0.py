97132cc59f7c4802233c212d29da4bfc248b99fd
Nicholas Griffin <113458048+N1ckGriffin@users.noreply.github.com>
2026-08-06 22:26:02 -0500

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,22 @@
+class TimeMap:
+
+    def __init__(self):
+        self.map = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.map[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        l, r = 0, len(self.map[key]) - 1
+        res = ""
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if self.map[key][m][0] <= timestamp:
+                res = self.map[key][m][1]
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return res
+
